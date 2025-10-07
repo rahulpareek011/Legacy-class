59aa0493fbc45e777abbe464a20487198eba615c
rahul pareek <itsrahul615@gmail.com>
2025-10-07 13:43:16 +0530

    Used Properties with enumeration

--- LegacyClasses/src/legacyclass/Demo.java
+++ LegacyClasses/src/legacyclass/Demo.java
@@ -4,17 +4,35 @@ import java.util.*;
 
 public class Demo {
 	public static void main(String[] args) {
-		//Using vector
-		Vector v = new Vector();
-		v.add("abd");
-		v.add("ald");
-		v.add("hgdk");
+//		//Using vector
+//		Vector v = new Vector();
+//		v.add("abd");
+//		v.add("ald");
+//		v.add("hgdk");
+//		
+//		Enumeration e = v.elements();
+//		
+//		while(e.hasMoreElements()) {
+//			System.out.println(e.nextElement());
+//		}
 		
-		Enumeration e = v.elements();
+		//Using Properties with enumeration
+		Properties p1 = new Properties();
+		p1.put("1","Java");
+		p1.put("2","React");
+		p1.put("3","DSA");
 		
-		while(e.hasMoreElements()) {
-			System.out.println(e.nextElement());
+		System.out.println(p1);
+		
+		Enumeration<?> e1 = p1.propertyNames();
+		
+		while(e1.hasMoreElements()) {
+			String keys = (String) e1.nextElement();
+			String values = p1.getProperty(keys);
+			System.out.println(keys + "-> " + values);
 		}
+		
+		
 	}
 
 }
